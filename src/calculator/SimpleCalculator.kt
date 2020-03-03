de695f8e9ab758cc364c328af0320d2adffb89a8
Wardi <dariusz.wardega@gmail.com>
2020-03-03 23:29:20 +0100

    Preparing Calculator Class - first step: primary constructor, first parameters, comments what's should be implement next one

--- src/calculator/SimpleCalculator.kt
+++ src/calculator/SimpleCalculator.kt
@@ -1,5 +1,25 @@
 package calculator
+/* Empty class:
+class [nameOfClass] - no body or
+ */
 
-class SimpleCalculator {
+
+/*
+Calculator class without primary constructor
+ */
+
+
+class SimpleCalculator constructor(val maxOperations: Byte){
+//    range of maxOperations <2,127> : less than 2 is non-sense, 127 is enough big number for simple calculations
+//      and additionally is maximal (positive) range of Byte
+    val maximumOperations: Byte = when{
+        maxOperations < 2 -> 2
+        maxOperations > 127 -> 127
+        else -> maxOperations
+    }
+    // Array of consecutive numbers
+    // var Numbers - Collection?
+    // Array of consecutive operations
+    // var Operations
 
 }
