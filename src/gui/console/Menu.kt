d250d4c4066fe0febe3574b97e544e3863f68342
Wardi <dariusz.wardega@gmail.com>
2020-03-03 10:13:56 +0100

    add comments with TODO tasks

--- src/gui/console/Menu.kt
+++ src/gui/console/Menu.kt
@@ -1,6 +1,9 @@
 package gui.console
 /*
 Main menu of console gui
+- how to exit a program
+- how to save history of our current calculations [serialization?]
+- how to start new calculation [ removing old history? removing when you try to save new one?]
  */
 class Menu {
 
