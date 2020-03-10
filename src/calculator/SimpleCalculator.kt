8956ddd83ba70b3e68c9b7f418f6af554f8410ee
Wardi <dariusz.wardega@gmail.com>
2020-03-10 23:00:08 +0100

    Add functions and field in SimpleCalculator class. Work in progress with 'calculate' function

--- src/calculator/SimpleCalculator.kt
+++ src/calculator/SimpleCalculator.kt
@@ -1,15 +1,12 @@
 package calculator
-/* Empty class:
-class [nameOfClass] - no body or
- */
-
 
 /*
 Calculator class without primary constructor
  */
 
+//class CannotComputeException : Exception();
 
-class SimpleCalculator constructor(val maxOperations: Byte){
+class SimpleCalculator constructor(private val maxOperations: Byte){
 //    range of maxOperations <2,127> : less than 2 is non-sense, 127 is enough big number for simple calculations
 //      and additionally is maximal (positive) range of Byte
     val maximumOperations: Byte = when{
@@ -17,9 +14,38 @@ class SimpleCalculator constructor(val maxOperations: Byte){
         maxOperations > 127 -> 127
         else -> maxOperations
     }
-    // Array of consecutive numbers
-    // var Numbers - Collection?
-    // Array of consecutive operations
-    // var Operations
+    //Collections of consecutives values and operators
+    private var values : MutableList<Double> = ArrayList<Double>()
+    private var operators: MutableList<String> = ArrayList<String>()
+    private var currentResult :Double = 0.0
+        get() = this.calculate()
+
+
+    public fun setNextValue(value: Double) : Unit {
+        this.values.add(value)
+    }
+
+    public fun setNextOperation(operator: String) : Unit {
+        this.operators.add(operator)
+    }
+
+    // to correct calculation quantity of operators should be less by 1 than quantity of values
+    private fun isQuantityCorrect() : Boolean {
+        return this.operators.size == this.values.size - 1
+    }
+
+    // In Progress
+    public fun calculate() : Double{
+        try {
+            if (!isQuantityCorrect())
+                throw Exception("Wrong quantity of operators or number values")
+        } catch (ex: Exception){
+            throw ex
+        } else {
+            for
+        }
+    }
+
+
 
 }
